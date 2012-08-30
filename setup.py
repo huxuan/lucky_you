0f7eb358200669db099ed802892b737495965e76
huxuan <i@huxuan.org>
2012-08-30 16:03:32 +0800

    Add setup.py for py2exe

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#encoding: utf8
+# +-----------------------------------------------------------------------------
+# | File: setup.py
+# | Author: huxuan
+# | E-mail: i(at)huxuan.org
+# | Created: 2012-03-09
+# | Last modified: 2012-03-09
+# | Description:
+# |     setup.py file of py2exe which will create exe file for win
+# |
+# | Copyrgiht (c) 2012 by huxuan. All rights reserved.
+# | License GPLv3
+# +-----------------------------------------------------------------------------
+from distutils.core import setup
+import py2exe
+
+setup(
+        name = "Lucky You",
+        version = "1.0",
+        options = { 
+            "py2exe" : {
+                "compressed" : 1,
+                "optimize" : 2,
+                "bundle_files" : 2,
+                "dll_excludes" : [ "MSVCP90.dll" ] }},
+        windows = ["lucky-you.py"],
+        zipfile = None,
+        author = "Hu Xuan",
+        author_email ="i@huxuan.org",
+        url = "http://github.com/huxuan/luck-you/",
+      )
